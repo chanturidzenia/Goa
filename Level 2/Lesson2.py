6a4234542c0a6185017305333571893d0437d2bf
chanturidzenia <niachanturidzee@gmail.com>
2024-05-16 17:20:22 +0400

    secondcommit

---
+++ Level 2/Lesson2.py
@@ -0,0 +1,59 @@
+name = "mariami"
+surname = "mchedlishvili"
+print(name +" "+ surname)
+
+name = "nika"
+surname = "datashvili"
+print(name + " " + surname)
+
+name = "nika"
+surname = "zhunzhunadze"
+print(name + " " + surname)
+
+name = "giorgi"
+surname = "tarielashvili"
+print(name+ " " + surname)
+
+name = "nika"
+surname = "fetelava"
+print(name + " " + surname)
+
+name = "rati"
+surname = "shavadze"
+print(name + " " + surname)
+
+name = "giorgi"
+surname = "varadashvili"
+print(name + " " + surname)
+
+name = "luca"
+surname = "guarnera"
+print(name + " " + surname)
+
+name = "mate"
+surname = "sapanadze"
+print(name + " " + surname)
+
+name = "levani"
+surname = "marciashvili"
+print(name + " " + surname)
+
+name = "anuki"
+surname = "shubladze"
+print(name + " " + surname)
+
+name = "nata"
+surname = "ksovreli"
+print(name + " " + surname)
+
+name = "barbare"
+surname = "tinikashvili"
+print(name + " " + surname)
+
+name = "saba"
+surname = "oqruashvili"
+print(name + " " + surname)
+
+name = "shotiko"
+surname = "nukradze"
+print(name + " " + surname)
